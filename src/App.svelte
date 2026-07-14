d8255eda273c502b1ae20d5157d59e54a422a2f6
Martin <mr.mmaurer@gmail.com>
2026-07-14 23:48:11 +0200

    Added grid/subgrid variant

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,24 @@
 <script lang="ts">
 </script>
 
-<main class="mx-auto max-w-3xl px-4 py-10">
+<!-- <header class="mx-auto max-w-3xl px-4 py-10">
   <h1 class="text-2xl font-semibold text-neutral-900">Home</h1>
+  <p>Text</p>
+</header> -->
+<main>
+  <section class="grid-container-subgrid">
+    <div class="grid-container-subgrid-row py-normal bg-brand-complementary">
+      <h2 class="text-xl text-brand-complementary-contrast font-bold">
+        Full bleed via grid and subgrid
+      </h2>
+    </div>
+    <div class="grid-container-subgrid-row-full-bleed bg-brand">
+      <div class="grid-container-subgrid-row py-normal">
+        <p class="text-lg text-brand-contrast">Full bleed</p>
+      </div>
+    </div>
+    <div class="grid-container-subgrid-row py-normal bg-brand-complementary">
+      <p class="text-lg text-brand-complementary-contrast">Normal</p>
+    </div>
+  </section>
 </main>
