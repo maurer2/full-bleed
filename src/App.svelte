4fc3db00b2849ad916417f0b8fd16fb12274eb14
Martin <mr.mmaurer@gmail.com>
2026-07-15 00:23:52 +0200

    Added container query units variant

--- src/App.svelte
+++ src/App.svelte
@@ -5,7 +5,7 @@
   <h1 class="text-2xl font-semibold text-neutral-900">Home</h1>
   <p>Text</p>
 </header> -->
-<main>
+<main class="flex flex-col gap-normal my-normal grid-lines-vertical">
   <section class="grid-container-subgrid">
     <div class="grid-container-subgrid-row py-normal bg-brand-complementary">
       <h2 class="text-xl text-brand-complementary-contrast font-bold">
@@ -18,6 +18,21 @@
       </div>
     </div>
     <div class="grid-container-subgrid-row py-normal bg-brand-complementary">
+      <p class="text-lg text-brand-complementary-contrast">Normal</p>
+    </div>
+  </section>
+  <section class="grid-container-query">
+    <div class="grid-container-query-row py-normal bg-brand-complementary">
+      <h2 class="text-xl text-brand-complementary-contrast font-bold">
+        Full bleed via container query units
+      </h2>
+    </div>
+    <div class="grid-container-query-row-full-bleed bg-brand">
+      <div class="grid-container-query-row py-normal">
+        <p class="text-lg text-brand-contrast">Full bleed</p>
+      </div>
+    </div>
+    <div class="grid-container-query-row py-normal bg-brand-complementary">
       <p class="text-lg text-brand-complementary-contrast">Normal</p>
     </div>
   </section>
