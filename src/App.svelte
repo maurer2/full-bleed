8bc36e6fdf8e977ee7115a08443b517cc45e88e0
Martin <mr.mmaurer@gmail.com>
2026-08-22 23:43:13 +0200

    Fixed oxfmt tailwind class sorting issue and applied recommended order

--- src/App.svelte
+++ src/App.svelte
@@ -5,10 +5,10 @@
   <h1 class="text-2xl font-semibold text-neutral-900">Home</h1>
   <p>Text</p>
 </header> -->
-<main class="flex flex-col gap-normal my-normal grid-lines-vertical">
+<main class="my-normal flex flex-col gap-normal grid-lines-vertical">
   <section class="grid-container-subgrid">
-    <div class="grid-container-subgrid-row py-normal bg-brand-complementary">
-      <h2 class="text-xl text-brand-complementary-contrast font-bold">
+    <div class="grid-container-subgrid-row bg-brand-complementary py-normal">
+      <h2 class="text-xl font-bold text-brand-complementary-contrast">
         Full bleed via grid and subgrid
       </h2>
     </div>
@@ -17,13 +17,13 @@
         <p class="text-lg text-brand-contrast">Full bleed</p>
       </div>
     </div>
-    <div class="grid-container-subgrid-row py-normal bg-brand-complementary">
+    <div class="grid-container-subgrid-row bg-brand-complementary py-normal">
       <p class="text-lg text-brand-complementary-contrast">Normal</p>
     </div>
   </section>
   <section class="grid-container-query">
-    <div class="grid-container-query-row py-normal bg-brand-complementary">
-      <h2 class="text-xl text-brand-complementary-contrast font-bold">
+    <div class="grid-container-query-row bg-brand-complementary py-normal">
+      <h2 class="text-xl font-bold text-brand-complementary-contrast">
         Full bleed via container query units
       </h2>
     </div>
@@ -32,21 +32,21 @@
         <p class="text-lg text-brand-contrast">Full bleed</p>
       </div>
     </div>
-    <div class="grid-container-query-row py-normal bg-brand-complementary">
+    <div class="grid-container-query-row bg-brand-complementary py-normal">
       <p class="text-lg text-brand-complementary-contrast">Normal</p>
     </div>
   </section>
 
   <section class="grid-container-border-shape">
-    <div class="grid-container-border-shape-row bg-brand-complementary py-normal">
-      <h2 class="text-xl text-brand-complementary-contrast font-bold">
+    <div class="bg-brand-complementary grid-container-border-shape-row py-normal">
+      <h2 class="text-xl font-bold text-brand-complementary-contrast">
         Full bleed via border-shape and border-color
       </h2>
     </div>
-    <div class="grid-container-border-shape-row-full-bleed border-brand py-normal">
+    <div class="border-brand grid-container-border-shape-row-full-bleed py-normal">
       <p class="text-lg text-brand-contrast">Full bleed</p>
     </div>
-    <div class="grid-container-border-shape-row bg-brand-complementary py-normal">
+    <div class="bg-brand-complementary grid-container-border-shape-row py-normal">
       <p class="text-lg text-brand-contrast">Normal</p>
     </div>
   </section>
